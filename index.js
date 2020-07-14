6ab3b588286adce9ce12abb0be66e02e3a9cccdb
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-14 20:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
 
   // Feel free to move things around!
-  const two = 2
+  n += 2;
+  return n;
+
 }
 
 var funkyFunction = function() {
